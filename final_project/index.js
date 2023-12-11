8aa01b5119c89042f55cb382f408e3f92026ec4f
keerat21 <48193090+keerat21@users.noreply.github.com>
2023-12-11 03:32:40 -0600

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,12 +11,30 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+     // Check if the user is authenticated
+    const token = req.headers.authorization;
+const isAuthenticated = verifyToken(token);
+if (isAuthenticated) {
+    // User is authenticated, proceed to the next middleware or route handler
+    next();
+  } else {
+    // User is not authenticated, send an unauthorized response
+    res.status(401).send("Unauthorized");
+  }
 });
+
+// Your other routes and middleware can go here
+
+// Example route using the authentication middleware
+// app.get("/customer/auth/profile", function (req, res) {
+//   res.send("User profile page");
+// });
+
+
  
-const PORT =5000;
+const PORT =3000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log(`Server is running at port {PORT}`));
